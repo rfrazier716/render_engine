a8c983d924713246089e042ca722b23040cb7ef0
RyanFrazier <rfrazier716@gmail.com>
2019-12-03 18:03:24 -0500

    moved to pipenv from pip and deleted requirements.txt. updated gitignore to ignore eclipse project settings. made a splines.py module which will be used to experiment with bezier curves

---
+++ splines.py
@@ -0,0 +1,19 @@
+'''
+Created on Dec 3, 2019
+
+@author: rfraz
+'''
+import numpy
+
+class Bezier(): 
+    # A 4 point Bezier Curve
+    def __init__(self,points):
+        #Pass in a list of tuples
+        pass
+    
+
+def main():
+    print("Hello World")
+    
+if __name__=="__main__":
+    main()
