8a87b4d0179291649bffff62aae12cfea1e090b4
RyanFrazier <rfrazier716@gmail.com>
2019-12-03 22:05:06 -0500

    working on bezier curve

--- splines.py
+++ splines.py
@@ -3,13 +3,15 @@ Created on Dec 3, 2019
 
 @author: rfraz
 '''
-import numpy
+import numpy as np
 
-class Bezier(): 
-    # A 4 point Bezier Curve
+class CubicBezier(): 
+    __m_basis_transform=np.array([[1,-3,3,-1],[0,3,-6,3],[0,0,3,3],[0,0,0,1]])
+    # A 4 point Bezier Curve, extendable for multiple dimensions
     def __init__(self,points):
+        self.bernstein_vector=points
+        self.polynomial_ve=np.matmul(np.linalg.inv(self.__m_basis_transform),self.bernstein_vector)
         #Pass in a list of tuples
-        pass
     
 
 def main():
