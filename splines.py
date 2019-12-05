5d4dcb5e3e253f83525042ea7ec4162a42566415
RyanFrazier <rfrazier716@gmail.com>
2019-12-05 07:41:31 -0500

    need to formalize how coordinates are passed to bezier curve. currently passing matrix of points which makes math efficient.

--- splines.py
+++ splines.py
@@ -9,12 +9,20 @@ class CubicBezier():
     __m_basis_transform=np.array([[1,-3,3,-1],[0,3,-6,3],[0,0,3,3],[0,0,0,1]])
     # A 4 point Bezier Curve, extendable for multiple dimensions
     def __init__(self,points):
-        self.bernstein_vector=points
-        self.polynomial_ve=np.matmul(np.linalg.inv(self.__m_basis_transform),self.bernstein_vector)
+        self.bernstein_matrix=points
+        self.polynomial_matrix=np.matmul(np.linalg.inv(self.__m_basis_transform),self.bernstein_matrix)
         #Pass in a list of tuples
+    
+    def get_bernstein_vector(self,dimension):
+        if dimension.lower() in "xyz":
+            #If a letter was provided, translate that to a coordinate and return
+            dimension='xyz'.find(dimension.lower())
     
 
 def main():
+    points=np.array([[0,0],[1,1],[2,-1],[3,0]])
+    bz=CubicBezier(points)
+    print(bz.polynomial_matrix)
     print("Hello World")
     
 if __name__=="__main__":
